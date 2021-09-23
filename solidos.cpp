3a23da591b1753b511a435afd0718580dbabeff6
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-09-23 17:30:10 -0500

    Creada clase para los bloques solidos del mapa

---
+++ solidos.cpp
@@ -0,0 +1,17 @@
+#include "solidos.h"
+
+solidos::solidos()
+{
+    h=0;
+    an=0;
+    posx=0;
+    posy=0;
+}
+
+solidos::solidos(int h_, int an_, int x, int y)
+{
+    h=h_;
+    an=an_;
+    posx=x;
+    posy=y;
+}
---
+++ solidos.h
@@ -0,0 +1,17 @@
+#ifndef SOLIDOS_H
+#define SOLIDOS_H
+
+
+class solidos
+{
+    int h;
+    int an;
+    int posx;
+    int posy;
+
+public:
+    solidos();
+    solidos(int h_, int an_, int x , int y);
+};
+
+#endif // SOLIDOS_H
