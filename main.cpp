9cf04aa88e2a488d112b973ee11e091d05a921a9
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-10-02 16:09:41 -0500

    Eliminada la nota

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@
 
 int main(int argc, char *argv[])
 {
-    /*
-     *NOTA: Las rutas a cada textura de los objetos
-     *se encuentra en la cabecera de los mismos
-    */
     QApplication a(argc, argv);
     Interfaz w;
     w.show();
