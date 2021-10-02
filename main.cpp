b53d2101522607f8aef3053afc675a4cc291181d
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-10-02 15:05:46 -0500

    Nota en el main

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 
 #include <QApplication>
 
+
 int main(int argc, char *argv[])
 {
+    /*
+     *NOTA: Las rutas a cada textura de los objetos
+     *se encuentra en la cabecera de los mismos
+    */
     QApplication a(argc, argv);
     Interfaz w;
     w.show();
